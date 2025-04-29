afc51b975ce96e983aea2fa350f1818299f937c5
Ishhod <ishhod419@gmail.com>
2025-04-28 22:28:01 -0400

    Most of the Logic created

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// const getRandomNumber = () => {
+//   let number = Math.floor(Math.random() * 100) + 1;
+//   return number;
+// };
+
+const number = ref(10);
+
+function incrementNumber() {
+  number.value += 1;
+}
+
+
+
+onMounted(() => {
+  let intervalId = setInterval(() => {
+    // number.value -= 1;
+    if (number.value > 0) {
+      number.value -= 1;
+    }// Decrease by 1 every second
+    
+  }, 500); //500 mmillisecond interval
+});
+
+// Clean up the interval when component is destroyed
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+ 
+
+
+
+</script>
+
+<template>
+
+  <body>
+    <div class="container">
+
+      <img src="./assets/PulsePoint.svg" alt="Vue logo" class="logo" />
+
+
+
+      <p>{{number}}</p>
+
+      <button @click="incrementNumber">Click Me</button>
+
+      <Teleport>
+        <p v-if="number >= 100" class="popupMessage">Congratulations Pleb</p>
+        <p v-if="number == 0" class="popupMessage">Tough Luck Buddy</p>
+      </Teleport>
+
+
+
+    </div>
+  </body>
+
+
+
+</template>
+
+
